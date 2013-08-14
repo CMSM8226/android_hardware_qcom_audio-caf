46e802b529f1562c2f4cefa6206772da60b83613
Jitendra Singh Naruka <jnaruk@codeaurora.org>
2013-08-14 14:11:28 -0700

    alsa_sound: enable stereo to custom stereo mixing at dsp
    During tunnel mode playback, based on a particular usr space
    setting stereo output's left and right channel are averaged in
    dsp, and the result is written back to both left and right
    channels. So, after averaging, the left and right channel are
    identical with the mono audio data. Added call to a mixer k-ctrl
    that enable/disables above in kernel.
    
    Change-Id: Id42442ccbacf4261307d2210acff16f65656a188

--- alsa_sound/AudioHardwareALSA.h
+++ alsa_sound/AudioHardwareALSA.h
@@ -130,6 +130,7 @@ class AudioHardwareALSA;
 #define AUDIO_PARAMETER_KEY_FM_VOLUME "fm_volume"
 #define ECHO_SUPRESSION     "ec_supported"
 #define ALL_CALL_STATES_KEY "all_call_states"
+#define CUSTOM_STEREO_KEY   "stereo_as_dual_mono"
 
 #define ANC_FLAG        0x00000001
 #define DMIC_FLAG       0x00000002
@@ -491,8 +492,8 @@ public:
                                 int param_id, int param_val);
     status_t setDDPEndpParams(alsa_handle_t *handle, int device, int dev_ch_cap,
                                char *ddpEndpParams, int *length, bool send_params);
-
     status_t getRMS(int *valp);
+    void setCustomStereoOnOff(bool flag);
 #ifdef SEPERATED_AUDIO_INPUT
     void     setInput(int);
 #endif
